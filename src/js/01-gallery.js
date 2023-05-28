75a8fc5c76fd42ec188e692c6636623c9e46c2a3
VadymKrem <vadimkuzin2@gmail.com>
2023-05-28 15:50:46 +0300

    dz8-1-3 try1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryCardsList = document.querySelector('.gallery'); 
+console.log(galleryCardsList);
+galleryCardsList.style.listStyle = 'none';
+
+galleryCardsList.innerHTML = createMarkupGalleryCardsList(galleryItems);
+// додавання розмітки галерії через функцію з аргументом обєкт
+
+function createMarkupGalleryCardsList(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) =>
+            `<li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+            <img class="gallery-card"
+             src="${preview}"
+            data-source= "${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>`)
+    .join("");
+}
+// функція створює розмітку галереї карток через масив з даних обєкта
+
+const lightbox = new SimpleLightbox('.gallery .gallery-item a', { 
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250
+});
+
+// підключення з документації лайтбокса, з options - вивід підпису, позиція, затримка 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form')
+}
+
+const getDataFromLocalStorage = localStorage.getItem("feedback-form-state");
+
+onReadDataForm();
+function onReadDataForm() {
+    if (getDataFromLocalStorage) {
+        refs.form.email.value = JSON.parse(getDataFromLocalStorage).email;
+        refs.form.message.value = JSON.parse(getDataFromLocalStorage).message;
+    } 
+}
+
+
+refs.form.addEventListener('input', throttle(onNewInputEmail, 500));
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onNewInputEmail() {
+    const dataForm = new FormData(refs.form)
+    const dataStorage = {};
+    dataForm.forEach((value, name) => {
+    dataStorage[name] = value;
+  });
+  localStorage.setItem("feedback-form-state", JSON.stringify(dataStorage));
+}
+
+function onSubmitForm(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+}
